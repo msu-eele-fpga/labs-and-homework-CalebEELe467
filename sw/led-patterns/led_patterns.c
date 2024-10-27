f637720619f3d4c89ed7156302a5d52c357a2f77
Caleb4884 <147229305+Caleb4884@users.noreply.github.com>
2024-10-27 15:41:33 -0600

    Created led_patterns.c

---
+++ sw/led-patterns/led_patterns.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <sys/mman.h> // for mmap
+#include <fcntl.h>    // for file open flags
+#include <unistd.h>   // for getting the page size
+#include <getopt.h>
+
+void usage()
+{
+    fprintf(stderr, "")
+}
