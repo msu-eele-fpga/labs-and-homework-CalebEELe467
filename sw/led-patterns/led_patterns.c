19d57fec69f34e7890bf6f73671e60a3fb40a7c7
Caleb4884 <147229305+Caleb4884@users.noreply.github.com>
2024-10-28 13:29:09 -0600

    Started Parsing inputs for led_patterns.c

--- sw/led-patterns/led_patterns.c
+++ sw/led-patterns/led_patterns.c
@@ -5,7 +5,6 @@
 #include <sys/mman.h> // for mmap
 #include <fcntl.h>    // for file open flags
 #include <unistd.h>   // for getting the page size
-#include <getopt.h>
 
 void usage()
 {
@@ -17,45 +16,31 @@ void usage()
     fprintf(stderr, "\t-v\t Verbose Mode. Prints LED Pattern in binary and the display duration");
     fprintf(stderr, "\t-p\t Set the LED Pattern and duration");
     fprintf(stderr, "\t-f\t Load LED Patterns and durations from a text file");
-
 }
 
 int main(int argc, char **argv)
 {
-    char c;
-    int digit_optind = 0;
-    int aopt = 0, bopt = 0;
-    char *copt = 0, *dopt = 0;
-    while ((c = getopt(argc, argv, "hvf:p:")) ! = -1){
+    int c;
 
-        
+    int hflag, vflag, fflag, pflag = 0;
+    char *fvalue, *pvalue = NULL;
+
+    while ((c = getopt(argc, argv, "hvf:p:")) ! = -1)
+    {
+        switch (c)
+        {
+        case 'h':
+            hflag = 1;
+            break;
+        case 'v':
+            vflag = 1;
+            break;
+        case 'f':
+            fflag = 1;
+            break;
+        case 'p':
+            pflag = 1;
+            break;
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
