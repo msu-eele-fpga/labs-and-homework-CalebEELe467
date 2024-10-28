e487d340609c9849722159060c86588b68344eec
Caleb Binfet <caleb.binfet@student.montana.edu>
2024-10-27 19:11:35 -0600

    added getopt to led_patterns.c

--- sw/led-patterns/led_patterns.c
+++ sw/led-patterns/led_patterns.c
@@ -9,5 +9,53 @@
 
 void usage()
 {
-    fprintf(stderr, "")
+    fprintf(stderr, "led_patterns [OPTION]... [ARGUMENT]...\n");
+    fprintf(stderr, "Control FPGA LED Outputs\n");
+    fprintf(stderr, "Example: led_patterns -v stuff Here");
+    fprintf(stderr, "Input Arguments:\n");
+    fprintf(stderr, "\t-h\t Print this help Menu and exit");
+    fprintf(stderr, "\t-v\t Verbose Mode. Prints LED Pattern in binary and the display duration");
+    fprintf(stderr, "\t-p\t Set the LED Pattern and duration");
+    fprintf(stderr, "\t-f\t Load LED Patterns and durations from a text file");
+
+}
+
+int main(int argc, char **argv)
+{
+    char c;
+    int digit_optind = 0;
+    int aopt = 0, bopt = 0;
+    char *copt = 0, *dopt = 0;
+    while ((c = getopt(argc, argv, "hvf:p:")) ! = -1){
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
