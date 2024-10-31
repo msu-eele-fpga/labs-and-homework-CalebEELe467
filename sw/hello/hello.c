057f8bfeaed927fa90d20be4c9fe32e69912261f
Caleb Binfet <caleb.binfet@student.montana.edu>
2024-10-30 21:49:35 -0600

    Finished Lab 3

--- sw/hello/hello.c
+++ sw/hello/hello.c
@@ -3,6 +3,7 @@
 int main(){
 	printf("Hello World\n");
 	printf("My Name is Caleb\n");
+	printf("Manual Compilation\n");
 	return 0;
 
 }
--- sw/led-patterns/led_patterns.c
+++ sw/led-patterns/led_patterns.c
@@ -12,10 +12,10 @@ void usage()
     fprintf(stderr, "Control FPGA LED Outputs\n");
     fprintf(stderr, "Example: led_patterns -v stuff Here");
     fprintf(stderr, "Input Arguments:\n");
-    fprintf(stderr, "\t-h\t Print this help Menu and exit");
-    fprintf(stderr, "\t-v\t Verbose Mode. Prints LED Pattern in binary and the display duration");
-    fprintf(stderr, "\t-p\t Set the LED Pattern and duration");
-    fprintf(stderr, "\t-f\t Load LED Patterns and durations from a text file");
+    fprintf(stderr, "\t-h\t Print this help Menu and exit\n");
+    fprintf(stderr, "\t-v\t Verbose Mode. Prints LED Pattern in binary and the display duration\n");
+    fprintf(stderr, "\t-p\t Set the LED Pattern and duration\n");
+    fprintf(stderr, "\t-f\t Load LED Patterns and durations from a text file\n");
 }
 
 int main(int argc, char **argv)
@@ -24,10 +24,10 @@ int main(int argc, char **argv)
 
     int vflag, fflag, pflag = 0;
     char *fvalue, *pvalue = NULL;
-
-    while ((c = getopt(argc, argv, "hvf:p:")) ! = -1)
+        //NOtes for me : argc stores input options +1
+    while ((c = getopt(argc, argv, "hvf:p:")) != -1)
     {
-        fprintf(stderr, "arguments %d", argc);
+        fprintf(stderr, "arguments %d \n", argc);
         switch (c)
         {
         case 'h': // Prints Help Menu
@@ -35,12 +35,18 @@ int main(int argc, char **argv)
             break;
         case 'v': // Verbose Mode
             vflag = 1;
+     
             break;
         case 'f': // Read file
             fflag = 1;
             break;
         case 'p': // Manual Pattern Input
             pflag = 1;
+            optind--;
+            for( ;optind < argc && *argv[optind] != '-'; (optind+= 2)){
+                fprintf(stderr, "the hex value is %s \n", argv[optind]);
+                fprintf(stderr, "the time value is %d \n", argv[optind++]);
+            }
             break;
         }
     }
